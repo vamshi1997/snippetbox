28e228aab0d8da55655636c06d9e3dd8b993ef9b
vamshi1997 <vamshikrishna0997@gmail.com>
2024-12-18 13:36:47 +0530

    adding router + new home endpoint

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Welcome to SnippetBox !!!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Println("Server listening on port 4000")
+
+	err := http.ListenAndServe(":4000", mux)
+	if err != nil {
+		log.Fatal(err)
+		panic(err)
+	}
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from snippetbox"))
 }
